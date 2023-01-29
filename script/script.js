261b93a3792810dd0b79d9cc9f89bbea1916831c
https://github.com/marija-rajak <mrajchica@gmail.com>
2023-01-29 21:03:31 +0100

    Responsive design added Modal design added Display buttons added

--- script/script.js
+++ script/script.js
@@ -4,8 +4,35 @@ const images = document.getElementsByClassName('image-small');
 const modal = document.getElementById('modal');
 const closeBtn = document.getElementById('closeButton');
 const modalContent = document.getElementById('modal-content');
+const gridBtn = document.getElementById('gridView');
+const listBtn = document.getElementById('listView');
+const containers = document.getElementsByClassName('img-container');
 
-fetch("https://api.unsplash.com/photos/random?count=12", {
+listBtn.addEventListener('click', function () {
+	mainContainer.style.flexFlow = 'column nowrap';
+	mainContainer.style.alignItems = 'center';
+	gridBtn.disabled = false;
+	listBtn.disabled = true;
+	for (let i = 0; i < containers.length; i++){
+		containers[i].style.width = '80%';
+		images[i].style.height = '400px';
+	}
+})
+
+gridBtn.addEventListener('click', function () {
+	mainContainer.style.flexFlow = 'row wrap';
+	gridBtn.disabled = true;
+	listBtn.disabled = false;
+	for (let i = 0; i < containers.length; i++){
+		containers[i].style.width = 'calc(33% - 20px)';
+		images[i].style.height = '300px';
+	}
+})
+
+loadImages();
+
+function loadImages() {
+	fetch("https://api.unsplash.com/photos/random?count=12", {
 	method: 'GET',
 	headers: {
 		'Authorization': 'Client-ID yqxi9saF9vPwq1NeG2S2FiLRV5hsbghDOe0qbMijLpY'
@@ -17,32 +44,9 @@ fetch("https://api.unsplash.com/photos/random?count=12", {
 }).then(function (images) {
 	images.forEach(function (image) {
 
-		/* mainContainer.innerHTML += `
-		<div class="img-container">
-			<div class="frame">
-				<img src="${image.urls.small}" alt="${createAlt(image)}" class="image-small" id="${image.id}>
-				<a href="https://unsplash.com" class="unsplLink">Unsplash</a>
-			</div>
-			<div>
-				<p class="likes"><span>heart</span> ${image.likes}</p>
-				<p class="downloads"><span>arrow</span> ${image.downloads}</p>
-			</div>
-			<div class="photographer">
-				<p>Photographer: ${image.user.username}</p>
-				<img src="${image.user.profile_image.small}" alt="${image.user.username}">
-			</div>
-			<div>
-				<a href="${image.user.links.portfolio}">Portfolio</a>
-				<p class="social">${image.user.social.instagram_username}, ${image.user.social.twitter_username}</p>
-			</div>
-		</div>` */
-
 		mainContainer.innerHTML += `
 		<div class="img-container">
-
 			<img src="${image.urls.small}" alt="${createAlt(image)}" class="image-small" id="${image.id}">
-			<a href="https://unsplash.com" class="unsplLink">Unsplash</a>
-
 		</div>`
 
 	});
@@ -52,6 +56,7 @@ fetch("https://api.unsplash.com/photos/random?count=12", {
 	loader.style.display = 'none';
 	mainContainer.style.display = 'flex';
 });
+}
 
 function createAlt(imageObj) {
 	if (imageObj.alt_description) {
@@ -71,29 +76,26 @@ function addEL() {
 function openModal() {
 	let id = this.id;
 
-	fetch("https://api.unsplash.com/photos?id=" + id + "&client_id=yqxi9saF9vPwq1NeG2S2FiLRV5hsbghDOe0qbMijLpY")
+	fetch("https://api.unsplash.com/photos/" + id + "?client_id=yqxi9saF9vPwq1NeG2S2FiLRV5hsbghDOe0qbMijLpY")
 		.then(function (response) {
 			return response.json();
 		})
 		.then(function (image) {
 			modalContent.innerHTML = `
-
 			<img src="${image.urls.full}" alt="">
+			<a href="${image.links.download}" class="unsplLink">Click to download</a>
 			<div class="img-info">
-				<div>
-					<p class="likes"><span>heart</span> ${image.likes}</p>
-					<p class="downloads"><span>arrow</span> ${image.downloads}</p>
-				</div>
+				<div class="likes"><i class="fa-solid fa-heart"></i> ${image.likes}</div>
+				<div class="downloads"><i class="fa-solid fa-download"></i> ${image.downloads}</div>
 				<div class="photographer">
-					<p>Photographer: ${image.user.username}</p>
+					<p>Photographer: ${image.user.username} </p>
 					<img src="${image.user.profile_image.small}" alt="${image.user.username}">
 				</div>
-				<div>
-					<a href="${image.user.links.portfolio}">Portfolio</a>
-					<p class="social">${image.user.social.instagram_username}, ${image.user.social.twitter_username}</p>
-				</div>
-			</div>
-			`
+				<a href="${image.user.links.portfolio}">Portfolio</a>
+				<a href="https://www.instagram.com/${createLink(image.user.social.instagram_username)}><i class="fa-brands fa-instagram"></i></a>
+				<a href="https://www.twitter.com/${createLink(image.user.social.twitter_username)}><i class="fa-brands fa-twitter"></i></a>
+			</div>`
+
 		});
 	modal.style.display = 'block';
 }
@@ -107,3 +109,18 @@ window.onclick = function (event) {
 		modal.style.display = 'none';
 	}
 }
+
+function createLink(property) {
+	lnk = property;
+	if (lnk) {
+		return lnk + '"';
+	} else {
+		return lnk + '"' + 'class="disabled"';
+	}
+}
+
+window.addEventListener('scroll', function () {
+	if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+		loadImages();
+	}
+});
